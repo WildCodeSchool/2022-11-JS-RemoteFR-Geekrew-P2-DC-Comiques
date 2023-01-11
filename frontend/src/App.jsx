49b767b4bb90017005a9ee35c7c56bcd2d078292
GessyBoy Yohann <ydeletrez@gmail.com>
2023-01-11 16:35:32 +0100

    clean branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import Home from "./pages/Home";
-
-
-import Crash from "./pages/Crash";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-
       <Home />,
-
     </div>
   );
 }
