afdeda8cc7b8b9b698b8eb773049b23b4d66a079
Baptcave <[noye.baptiste@orange.fr](mailto:noye.baptiste@orange.fr)>
2023-01-11 16:39:11 +0100

    deleted the breaks around Home and the import of crash page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import Home from "./pages/Home";
 
-
-import Crash from "./pages/Crash";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-
       <Home />,
-
     </div>
   );
 }
