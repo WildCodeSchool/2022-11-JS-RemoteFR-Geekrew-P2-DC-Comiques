2838b19257b9ea29267480d62e6da966a00ae51e
Jean-Philippe Roblot <jphilippe.roblot@gmail.com>
2023-01-11 21:59:16 +0100

    fix Game.jsx with import data.json

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,25 +1,16 @@
 import "../styles/Game.module.css";
 import ClassicCard from "@components/ClassicCard";
+import data from "../data/data.json";
 
 function Game() {
-  const cardTest = {
-    id: "sc001",
-    imageSource: "src/assets/scene_LaboFigma.png",
-    title: "Initialisation",
-    text: "Grâce au réducteur-reducer, vous êtes numérisé et injecté dans le code vérolé à sauver. Vous atterrissez sur la landing page mais une immense code-erreur terrifiante se dresse devant vous et vous menace. No need to panick ! Juste to react…",
-    choices: [
-      "Vous clonez le code",
-      "Vous lui envoyez ctrl+alt+suppr dans son immense gueule dentée",
-    ],
-    popUpText: [],
-  };
+ 
 
   return (
     <div>
       <ClassicCard
-        imageSource={cardTest.imageSource}
-        title={cardTest.title}
-        text={cardTest.text}
+        imageSource={data.scenarioCards[0].imageSource}
+        title={data.scenarioCards[0].title}
+        text={data.scenarioCards[0].text}
       />
     </div>
   );
