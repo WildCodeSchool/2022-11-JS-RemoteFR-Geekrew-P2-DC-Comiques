95743e72297d65e9a49d8c694a7fd0c17bc5eadc
Jean-Philippe Roblot <jphilippe.roblot@gmail.com>
2023-01-12 16:10:32 +0100

    update data.json, change choices objects

--- frontend/src/components/ClassicCard.jsx
+++ frontend/src/components/ClassicCard.jsx
@@ -2,15 +2,15 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 import styles from "../styles/Game.module.css";
 
-function ClassicCard({ imageSource, title, text }) {
+function ClassicCard({ imageSource, title, text, choices }) {
   return (
     <div className={styles.main}>
       <img src={imageSource} className={styles.img} alt="Labo" />
       <h1>{title}</h1>
       <p>{text}</p>
       <div className={styles.choice}>
-        <Button />
-        <Button />
+        <Button choice={choices.choice1.text} />
+        <Button choice={choices.choice2.text} />
       </div>
     </div>
   );
@@ -20,6 +20,7 @@ ClassicCard.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   imageSource: PropTypes.string.isRequired,
+  choices: PropTypes.shape.isRequired,
 };
 
 export default ClassicCard;
